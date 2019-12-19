4ce86c80dd984fab90d6855594e1df0d5da02402
Matt Peterson <themattman18@outlook.com>
2019-12-19 14:46:36 -0600

    Added data to results screen

--- ManagerOfTheYear/ManagerOfTheYear/Pages/Results.cshtml.cs
+++ ManagerOfTheYear/ManagerOfTheYear/Pages/Results.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace ManagerOfTheYear.Pages
 {
@@ -16,10 +17,42 @@ namespace ManagerOfTheYear.Pages
             _connection = connection.Value;
         }
 
-        public IActionResult OnGet()
+        public void OnGet()
         {
-            return Page();
+            int total = 0;
+            int davidCount = 0;
+            int tommyCount = 0;
 
+            using (SqlConnection con = new Microsoft.Data.SqlClient.SqlConnection(_connection.DefaultConnection))
+            {
+                con.Open();
+                
+                using (SqlCommand sqlCommand = new SqlCommand("select count(*) as cnt, vote from votes " + 
+                                                                " where vote = 1 or vote = 2 " + 
+                                                                " group by vote", con))
+                {
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader[1].ToString() == "1")
+                        {
+                            davidCount = (int)reader[0];
+                            ViewData["DavidCount"] = reader[0].ToString();
+                        }
+                        else
+                        {
+                            tommyCount  = (int)reader[0];
+                            ViewData["TommyCount"] = reader[0].ToString();
+                        }
+                    }
+                }
+            }
+
+            total = davidCount + tommyCount;
+
+            ViewData["DavidPercentage"] = (int)(Math.Round((decimal)davidCount / (decimal)total, 2) * 100);
+            ViewData["TommyPercentage"] = (int)(Math.Round((decimal)tommyCount / (decimal)total, 2) * 100);
+            ViewData["Total"] = total;
         }
     }
 }
